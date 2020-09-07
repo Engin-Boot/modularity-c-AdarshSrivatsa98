3c96ddb6d1cde82015a37f7625c9047b0a0f47e1
AdarshSrivatsa98 <adarsh.98srivatsa@gmail.com>
2020-09-07 21:04:20 +0530

    Modularity by creation of libraries

---
+++ TelCo.ColorCoder/Libraries/ColorDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+
+namespace TelCo.ColorCoder.Libraries
+{
+    class ColorDefination
+    {
+        public static readonly Color[] colorMapMajor;
+        public static readonly Color[] colorMapMinor;
+        static ColorDefination()
+        {
+            colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
+            colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
+        }
+    }
+}
